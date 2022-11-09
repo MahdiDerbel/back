f892e60bda9fcd914bafcd56e3d3826763a3d743
mahdiderbel <mohamed.mahdi.derbel@gmail.com>
2022-11-09 14:21:05 +0100

    first commit

---
+++ server/config.js
@@ -0,0 +1,16 @@
+var config = {
+  base: "opalia2",
+  root: "root",
+  host: "localhost",
+  password: "",
+  port: "3306", 
+
+  /* base: "opalia",
+  root: "opalia", 
+  host: "localhost",
+  password: "Opalia.2021+",
+  port: "3306", */
+};
+module.exports = {
+  connection: config,
+};
